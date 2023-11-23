ac07275c68a6c2adc7b371e1a809ea6c138c1c13
dean <nicklasfrederiksen@gmail.com>
2023-11-23 13:22:08 +0100

    first

--- file-example.js
+++ file-example.js
@@ -1,1 +1,5 @@
 const fs = require("fs");
+
+const data = fs.readFileSync("data.json","utf-8");
+const dataAsJson = JSON.parse(data);
+console.log(dataAsJson[0].productName);
--- http-example.js
+++ http-example.js
@@ -1,4 +1,7 @@
 const http = require("http");
 
 //write & end
-http.createServer((req, res)=>{})
+http.createServer((req, res)=>{
+    res.write("Hello Everyone");
+    res.end();
+}).listen(3000);
