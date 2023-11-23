d8fd604c08f08d62ab53076aab17fc32b8dfbf2c
dean <nicklasfrederiksen@gmail.com>
2023-11-23 14:22:48 +0100

    first

--- express-example.js
+++ express-example.js
@@ -4,7 +4,25 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get("/",(req, res) => {});
+app.get("/",(req, res) => {
+   res.send("🦺 Hey hey ");
+});
+
+app.get("/greetings",(req, res) => {
+   res.send("Hey everyone thanks for visiting my very first API 🐕‍🦺")
+});
+
+//Get a product by name
+app.get("/products/:name", (req, res) =>{
+   let dataAsString = fs.readFileSync("data.json","utf-8");
+   let dataAsJson = JSON.parse(dataAsString);
+   for (let i = 0; i < dataAsJson.length; i++) {
+      if(dataAsJson[i].productName == req.params.name){
+         res.send(dataAsJson[i]);
+      }
+   }
+   res.send("404 product not found");
+});
 
 //Code that initiates and runs the express application
 app.listen(port, () => {
